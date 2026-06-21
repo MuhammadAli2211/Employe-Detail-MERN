e8440038579cd82d2ea930ac6e046ee4d6106d9d
MuhammadAli2211 <moonansari936@gmail.com>
2026-06-21 20:42:19 +0500

    Fix CORS policy issue for vercel

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ require("dotenv").config();
 const app = express();
 
 
-app.use(cors()); 
+app.use(cors({
+  origin: "https://employe-detail-mern-raqj.vercel.app", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 
